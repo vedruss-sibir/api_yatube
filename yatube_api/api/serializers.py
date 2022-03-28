4fc6a11b822debd47a0a1fe0d3ffa5c5a7e9dfcf
Андрей Стрельников <andre33_3@rambler.ru>
2022-03-28 09:45:57 +0700

    m

--- yatube_api/api/serializers.py
+++ yatube_api/api/serializers.py
@@ -15,7 +15,7 @@ class CommentSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Comment
-        fields = "__all_"
+        fields = "__all__"
 
 
 class GroupSerializer(serializers.ModelSerializer):
